4f620a65b34bcdbada9c346971776e9741769fcc
50mkw <50mkw@users.noreply.github.com>
2026-02-19 01:33:38 +0000

    Add Remotion video bundle for employee management system

---
+++ 845.bundle.js
@@ -0,0 +1,11 @@
+(self["webpackChunkopenclaw_remotion_tutorial"] = self["webpackChunkopenclaw_remotion_tutorial"] || []).push([[845],{
+
+/***/ 1845
+() {
+
+/* (ignored) */
+
+/***/ }
+
+}]);
+//# sourceMappingURL=845.bundle.js.map
